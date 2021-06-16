6bd943138a5da2a39eb5cd9f013d2227cf1c1c5a
Senayson <senaialema@gmail.com>
2021-06-16 20:06:29 +0000

    Added switch color to red when negative feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,24 @@
+const counterVar = 
+
 $(document).ready(function() {
   console.log("Document loaded");
 
   $("#tweet-text").on('input', function(){
-    const val = $(this).val().length;
-    const counter= $(this).parent().find('.counter').val();
-    $('.counter').val(counter - val);
-  })
-  
+    //Input length
+    const inputVal = $(this).val().length;
+    //
+    const $counter= $(this).parent().find('.counter');
+    let counterVal = 140;
+     counterVal -= inputVal;
+    console.log(counterVal);
 
-});
+   $counter.val(counterVal);
+  
+    if(counterVal < 0) {
+      $('.counter').addClass('negative');
+    }
+
+
+   })
+
+})
