a0518dc5ed1af5bd9abdca0af955def458b84b9e
Senayson <senaialema@gmail.com>
2021-06-16 19:31:59 +0000

    Implemented counter fucntion

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   console.log("Document loaded");
 
- 
+  $("#tweet-text").on('input', function(){
+    const val = $(this).val().length;
+    const counter= $(this).parent().find('.counter').val();
+    $('.counter').val(counter - val);
+  })
   
 
 });
