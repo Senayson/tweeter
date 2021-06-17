9e3fe96e34b24157c2c40f4efa83883451a4dab9
Senayson <senaialema@gmail.com>
2021-06-17 12:46:56 +0000

    RenderTweets function implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,30 @@
  */
 $(document).ready(function() {
 
-const tweetObj = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 const createTweetElement = function(tweetObj) {
 const $tweet = $(`
@@ -32,13 +45,25 @@ const $tweet = $(`
           </div>
         </footer>
       </article>`);
-
-
-
 return $tweet;
 };
-const $tweet = createTweetElement(tweetObj);
-$('.tweetContainer').append($tweet);
-console.log($tweet);
+// const $tweet = createTweetElement(tweetObj);
+// $('.tweetContainer').append($tweet);
+// console.log($tweet);
+
+
+
+const renderTweets = function(data) {
+
+  for(const tweet of data) {
+   const $tweet = createTweetElement(tweet);
+   console.log("This is each tweet:" ,$tweet);
+   $('.tweetContainer').append($tweet);
+
+  }
+
+}
+
+renderTweets(data);
 
 });
